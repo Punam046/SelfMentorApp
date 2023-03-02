283a115d390c30e8e9dd53994922dc9915e33791
Punam046 <kwrpunam046@gmail.com>
2023-03-02 01:53:21 -0800

    Files added

---
+++ app/src/main/java/com/punam/selfmentor1/QuestionAnswer.java
@@ -0,0 +1,19 @@
+package com.punam.selfmentor1;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "What is your name?",
+            "What is your age?",
+            "What is your address?"
+    };
+    public static String choices[][]={
+            {"punam","Kunwar","suru","manju"},
+            {"punam","Kunwar","suru","manju"},
+            {"punam","Kunwar","suru","manju"}
+    };
+    public static String correctAnswer[]={
+            "punam",
+            "suru",
+            "manju"
+    };
+}
